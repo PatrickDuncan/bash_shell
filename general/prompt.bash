01ddd4c209b011dcea79c93b1d230ea59a55b8b0
Patrick <patrick.duncan@outlook.com>
2018-06-10 18:19:07 -0400

    Change prompt colour depending on the weekday

--- general/prompt.bash
+++ general/prompt.bash
@@ -6,9 +6,12 @@ complete -d cd
 complete -d rmdir
 
 # Prompt visuals
-BRANCH() {
+BRANCH() { # Get current git branch
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-export PS1="\[\033[1;33m\]\W\[\033[1;36m\]\$(BRANCH)\[\033[1;33m\]$\[\033[m\] "
+FOLDER_COLOR() { # Change the folder and $ colour depending on the weekday
+  expr $(date +%u) - 2
+}
+export PS1="\[\033[1;3\$(FOLDER_COLOR)m\]\W\[\033[1;36m\]\$(BRANCH)\[\033[1;3\$(FOLDER_COLOR)m\]$\[\033[m\] "
 export CLICOLOR=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
