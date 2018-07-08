a63debf8ad96efef91d0b4424eea72c10367e725
Patrick Duncan <patrick.duncan@outlook.com>
2018-07-07 21:57:01 -0400

    Pretty git log and moved open to mac

--- mac/aliases.bash
+++ mac/aliases.bash
@@ -2,7 +2,7 @@
 alias atom='open -a 'Atom' .'
 
 # I
-alias ip="ifconfig | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'"
+alias ip="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1'"
 
 # L
 alias ll='ls -GFhAlT'
